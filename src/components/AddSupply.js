e48cef3b11b15344c1b5363c1bc404699057b904
mathena828 <mathena828@gmail.com>
2020-04-04 14:31:48 +0800

    handleSubmit Function Added

--- src/components/AddSupply.js
+++ src/components/AddSupply.js
@@ -1,78 +1,96 @@
-import React, { useState } from 'react';
-import {useStep} from 'react-hooks-helper';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import OrgForm from './OrgForm.js';
-import SupplyForm from './SupplyForm.js';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useStep } from "react-hooks-helper";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import OrgForm from "./OrgForm.js";
+import SupplyForm from "./SupplyForm.js";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const steps = [
-    { id: "organization" },
-    { id: "supplies" },
-    { id: "submit" }
-  ];
+const steps = [{ id: "organization" }, { id: "supplies" }, { id: "submit" }];
 
-function handleSubmit(orgName, firstName,lastName,email, phone, supplies){
-    console.log("test");
-    if (orgName==='' || firstName==='' || lastName==='' || phone===''){
-        alert("Please ensure that you have filled out all fields in the form.");
-        return false;
-    }
-    
-    var name = firstName + " " + lastName;
-    let body = {organization:orgName,
-                name: name, 
-                mobile:phone,
-                email: email};
-    fetch("http://localhost:8000/api/supplier/", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify(body)
-    }).then((response)=>{
-        return response.json();
-    }).then((data) => {
-        console.log(data);
-    });
+function handleSubmit(orgName, firstName, lastName, email, phone, supplies) {
+  const supplier = {
+    organization: orgName,
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    mobile: phone
+  };
+  axios.post("http://localhost:8000/api/supplier/", supplier);
+  for (var i = 0; i < supplies.length; i++) {
+    const supply = {
+      supplier: orgName,
+      name: supplies[i].index,
+      region: supplies[i].region,
+      address: supplies[i].address,
+      quantity: supplies[i].quantity,
+      unit: supplies[i].unit,
+      price: supplies[i].price,
+      description: supplies[i].description
+    };
+    axios.post("http://localhost:8000/api/supply/", supply);
+  }
 }
-export default function AddSupply(){
-    const {step, navigation} = useStep({ initialStep: 0, steps });
-    /* const [formData, setForm] = (defaultData); */
-   /*  const props2 = { formData, setForm, navigation }; */
-    
-    const [orgName, setOrgName] = useState('Independent');
-    const [firstName, setFirstName] = useState('Juan');
-    const [lastName, setLastName] = useState('Dela Cruz');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState(''); 
-    const props = {orgName, setOrgName, firstName, setFirstName, 
-        lastName, setLastName, email, setEmail, phone, setPhone,navigation};
-    
-    
-    const [supplies, setSupplies] = useState([]);
-    
-    const props2={orgName, firstName,lastName, email,phone, supplies, setSupplies, navigation, handleSubmit};
-    
-    
-    const { id } = step;
-    switch(id){
-        case "organization":
-            return <OrgForm {...props}/>;
-        case "supplies":
-            return <SupplyForm {...props2}/>
-        case "submit":
-            return(
-                <div>
-                    <Jumbotron>
-                        <h1>Thank you for your donation!</h1>
-                        <p>Thanks for helping out our LGUs. Please keep your email and phone at the ready if an LGU would like to purchase your supplies.</p>
-                        <Link to="/"><Button>Back To Home</Button></Link>
-                    </Jumbotron>
-                </div>
-            );
-        default:
-            return null;
-    }
+export default function AddSupply() {
+  const { step, navigation } = useStep({ initialStep: 0, steps });
+  /* const [formData, setForm] = useForm(defaultData); */
+  /*  const props2 = { formData, setForm, navigation }; */
+  const [orgName, setOrgName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const props = {
+    orgName,
+    setOrgName,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    phone,
+    setPhone,
+    navigation
+  };
+
+  const [supplies, setSupplies] = useState([]);
+
+  const props2 = {
+    orgName,
+    firstName,
+    lastName,
+    email,
+    phone,
+    supplies,
+    setSupplies,
+    navigation,
+    handleSubmit
+  };
+
+  const { id } = step;
+  switch (id) {
+    case "organization":
+      return <OrgForm {...props} />;
+    case "supplies":
+      return <SupplyForm {...props2} />;
+    case "submit":
+      return (
+        <div>
+          <Jumbotron>
+            <h1>Thank you for your donation!</h1>
+            <p>
+              Thanks for helping out our LGUs. Please keep your email and phone
+              at the ready if an LGU would like to purchase your supplies.
+            </p>
+            <Link to="/">
+              <Button>Back To Home</Button>
+            </Link>
+          </Jumbotron>
+        </div>
+      );
+    default:
+      return null;
+  }
 }
--- src/components/SupplyForm.js
+++ src/components/SupplyForm.js
@@ -1,194 +1,276 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
 //import Modal from 'react-bootstrap/Modal';
 
-import '../stylesheets/formStyle.css';
+import "../stylesheets/formStyle.css";
 
-var indexCount=0;
-const SupplyForm= ({ orgName, firstName,lastName, email,phone,supplies, setSupplies, navigation, handleSubmit })=>{
-    const [formRegion, setRegion] = useState('');
-    const [unit, setUnit] = useState('');
-    const [formName, setFormName] = useState('');
-    const [formAddress, setFormAddress] = useState('');
-    const [formAmount, setFormAmount] = useState(1);
-    const [formPrice, setFormPrice] = useState(0.00);
-    const [formDescription, setFormDescription] = useState('');
+var indexCount = 0;
+const SupplyForm = ({
+  orgName,
+  firstName,
+  lastName,
+  email,
+  phone,
+  supplies,
+  setSupplies,
+  navigation,
+  handleSubmit
+}) => {
+  const [formRegion, setRegion] = useState("");
+  const [unit, setUnit] = useState("");
+  const [formName, setFormName] = useState("");
+  const [formAddress, setFormAddress] = useState("");
+  const [formAmount, setFormAmount] = useState(1);
+  const [formPrice, setFormPrice] = useState(0.0);
+  const [formDescription, setFormDescription] = useState("");
 
-    const {previous} = navigation;
-    
-    //const [show, setShow] = useState(false);
+  const { previous } = navigation;
 
-    //const handleClose = () => setShow(false);
-    //const handleShow = () => setShow(true);
+  //const [show, setShow] = useState(false);
 
-    var supplyList=supplies.map((supply)=>{
-        var totalPrice = supply.price * supply.amount;
-        return(
-            <Card key={supply.index}>
-                
-                <Card.Body>
-                    <Card.Title>Item Name: {supply.name} 
-                    <div style={{textAlign:"right"}}>
-                        <Button onClick={()=>deleteSupply(supply.index)} variant = "outline-danger">
-                        <svg className="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z"/>
-                        <path fillRule="evenodd" d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" clipRule="evenodd"/>
-                        </svg>
-                        </Button>
-                    </div>
-                    </Card.Title>
-                    <Card.Subtitle>Address: {supply.address}</Card.Subtitle>
-                    <hr/>
-                    <p>Description</p>
-                    <p className="supply-description">{supply.description}</p>
-                    <Card.Subtitle>{supply.amount} for Php{totalPrice}</Card.Subtitle>
-                </Card.Body>
-            </Card>
-        );
-    });
-    function saveSupply(name, address, region,amount,price,description){
-        var newSupply= {
-            name:name,
-            address:address,
-            amount:amount,
-            region:region,
-            price:price,
-            description:description,
-            index:indexCount
-        }
-        indexCount+=1;
-        setSupplies([...supplies, newSupply]);
-    }
+  //const handleClose = () => setShow(false);
+  //const handleShow = () => setShow(true);
 
-    function deleteSupply(supplyIndex){
-        const newSupplies = supplies.filter((_,index)=> index!==supplyIndex);
+  var supplyList = supplies.map(supply => {
+    var totalPrice = supply.price * supply.amount;
+    return (
+      <Card key={supply.index}>
+        <Card.Body>
+          <Card.Title>
+            Item Name: {supply.name}
+            <div style={{ textAlign: "right" }}>
+              <Button
+                onClick={() => deleteSupply(supply.index)}
+                variant="outline-danger"
+              >
+                <svg
+                  className="bi bi-trash"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M5.5 5.5A.5.5 0 016 6v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm2.5 0a.5.5 0 01.5.5v6a.5.5 0 01-1 0V6a.5.5 0 01.5-.5zm3 .5a.5.5 0 00-1 0v6a.5.5 0 001 0V6z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M14.5 3a1 1 0 01-1 1H13v9a2 2 0 01-2 2H5a2 2 0 01-2-2V4h-.5a1 1 0 01-1-1V2a1 1 0 011-1H6a1 1 0 011-1h2a1 1 0 011 1h3.5a1 1 0 011 1v1zM4.118 4L4 4.059V13a1 1 0 001 1h6a1 1 0 001-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Button>
+            </div>
+          </Card.Title>
+          <Card.Subtitle>Address: {supply.address}</Card.Subtitle>
+          <hr />
+          <p>Description</p>
+          <p className="supply-description">{supply.description}</p>
+          <Card.Subtitle>
+            {supply.amount} for Php{totalPrice}
+          </Card.Subtitle>
+        </Card.Body>
+      </Card>
+    );
+  });
+  function saveSupply(name, address, region, amount, price, description) {
+    var newSupply = {
+      name: name,
+      address: address,
+      amount: amount,
+      region: region,
+      price: price,
+      description: description,
+      index: indexCount
+    };
+    indexCount += 1;
+    setSupplies([...supplies, newSupply]);
+  }
 
-        setSupplies(newSupplies);
-    }
+  function deleteSupply(supplyIndex) {
+    const newSupplies = supplies.filter((_, index) => index !== supplyIndex);
 
-    return(
+    setSupplies(newSupplies);
+  }
 
-        <div className="SupplyForm">
-        <div>
-                <Jumbotron>
-                <Link to="/"><Button variant="outline-secondary">Back to Home</Button></Link>
-                   <h3>Hi, thank you for registering your produce to the site.
-                       Just answer this form and we'll be good to go!</h3> 
-                </Jumbotron>
-        </div>
-        <div className="container">
-            <Form onSubmit={(e)=>{handleSubmit(orgName, firstName,lastName, email,phone,supplies)}}>
-                <h3>Supply Information</h3>
-                <Form.Group className="container row">
-                    <Form.Control 
-                    value={formName}
-                    onChange={e=>setFormName(e.target.value)}
-                    type="text" 
-                    className="col-sm-5"
-                    placeholder="Item Name" />
-                </Form.Group>
-                <Form.Group>
-                    <div className="container">
-                        <div className="row">
-                            <Form.Control 
-                            value={formAddress}
-                            onChange={e=>setFormAddress(e.target.value)}
-                            type="text" placeholder="Address" 
-                            className="col-sm-7"/>
-                            <Form.Control 
-                            value={formRegion}
-                            as="select"
-                            onChange={e=>setRegion(e.target.value)}
-                            className="col-sm">
-                                <option>NCR - National Capital Region</option>
-                                <option>Region I - Ilocos Region</option>
-                                <option>Region II - Cagayan Valley</option>
-                                <option>CAR - Cordillera Administrative Region</option>
-                                <option>Region III - Central Luzon</option>
-                                <option>Region IV-A - Calabarzon</option>
-                                <option>Mimaropa - Southwestern Tagalog Region</option>
-                                <option>Region V - Bicol Region</option>
-                                <option>Region VI - Western Visayas</option>
-                                <option>Region VII - Central Visayas</option>
-                                <option>Region VIII - Eastern Visayas</option>
-                                <option>Region IX - Zamboanga Peninsula</option>
-                                <option>Region X - Northern Mindanao</option>
-                                <option>Region XI - Davao Region</option>
-                                <option>Region XII - Soccskargen</option>
-                                <option>Region XIII - Caraga Region</option>
-                                <option>BARMM - Bangsamoro Autonomous Region in Muslim Mindanao</option>
-                            </Form.Control>
-                        </div>
-                    </div>
-                   
-                    <Form.Text className="text-muted">
-                        Where will this item be available for pickup.
-                    </Form.Text>
-                </Form.Group>
+  return (
+    <div className="SupplyForm">
+      <div>
+        <Jumbotron>
+          <Link to="/">
+            <Button variant="outline-secondary">Back to Home</Button>
+          </Link>
+          <h3>
+            Hi, thank you for registering your produce to the site. Just answer
+            this form and we'll be good to go!
+          </h3>
+        </Jumbotron>
+      </div>
+      <div className="container">
+        <Form
+          onSubmit={e => {
+            handleSubmit(orgName, firstName, lastName, email, phone, supplies);
+          }}
+        >
+          <h3>Supply Information</h3>
+          <Form.Group className="container row">
+            <Form.Control
+              value={formName}
+              onChange={e => setFormName(e.target.value)}
+              type="text"
+              className="col-sm-5"
+              placeholder="Item Name"
+            />
+          </Form.Group>
+          <Form.Group>
+            <div className="container">
+              <div className="row">
+                <Form.Control
+                  value={formAddress}
+                  onChange={e => setFormAddress(e.target.value)}
+                  type="text"
+                  placeholder="Address"
+                  className="col-sm-7"
+                />
+                <Form.Control
+                  value={formRegion}
+                  as="select"
+                  onChange={e => setRegion(e.target.value)}
+                  className="col-sm"
+                >
+                  <option>NCR - National Capital Region</option>
+                  <option>Region I - Ilocos Region</option>
+                  <option>Region II - Cagayan Valley</option>
+                  <option>CAR - Cordillera Administrative Region</option>
+                  <option>Region III - Central Luzon</option>
+                  <option>Region IV-A - Calabarzon</option>
+                  <option>Mimaropa - Southwestern Tagalog Region</option>
+                  <option>Region V - Bicol Region</option>
+                  <option>Region VI - Western Visayas</option>
+                  <option>Region VII - Central Visayas</option>
+                  <option>Region VIII - Eastern Visayas</option>
+                  <option>Region IX - Zamboanga Peninsula</option>
+                  <option>Region X - Northern Mindanao</option>
+                  <option>Region XI - Davao Region</option>
+                  <option>Region XII - Soccskargen</option>
+                  <option>Region XIII - Caraga Region</option>
+                  <option>
+                    BARMM - Bangsamoro Autonomous Region in Muslim Mindanao
+                  </option>
+                </Form.Control>
+              </div>
+            </div>
 
-                <Form.Group>
-                    <Form.Label>Amount and Price</Form.Label>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <Form.Control 
-                                value={formAmount}
-                                onChange={e=>setFormAmount(e.target.value)}
-                                className="col-sm-4" type="number" placeholder="Amount" /> 
-                                <Form.Control className="col-sm-4" as="select" value={unit} onChange={e=>setUnit(e.target.value)}>
-                                    <option>g</option>
-                                    <option>kg</option>
-                                    <option>mL</option>
-                                    <option>liter</option>
-                                    <option>meter</option>
-                                    <option>item</option>
-                                </Form.Control>
-                            </div>
-                            <div className="col-sm-6">
+            <Form.Text className="text-muted">
+              Where will this item be available for pickup.
+            </Form.Text>
+          </Form.Group>
 
-                                <Form.Control type="number" placeholder="Price in Php" /> 
-                                <Form.Text 
-                                value={formPrice}
-                                onChange={e=>setFormPrice(e.target.value)}
-                                className="text-muted">
-                                    Answer price per unit.
-                                </Form.Text>
-                            </div>
-                        </div>
-                    </div>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control 
-                    placeholder="Place your description here"
-                    value={formDescription}
-                    onChange={e=>setFormDescription(e.target.value)}
-                    as="textarea" rows="3" />
-                </Form.Group>
-                    <Button onClick={e=> saveSupply(formName, formAddress,formRegion, formAmount,formPrice,formDescription, )}variant="primary">Add Item</Button>
-                    <Button type="submit" variant="danger">SUBMIT</Button>
-            </Form>
-        
+          <Form.Group>
+            <Form.Label>Amount and Price</Form.Label>
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6">
+                  <Form.Control
+                    value={formAmount}
+                    onChange={e => setFormAmount(e.target.value)}
+                    className="col-sm-4"
+                    type="number"
+                    placeholder="Amount"
+                  />
+                  <Form.Control
+                    className="col-sm-4"
+                    as="select"
+                    value={unit}
+                    onChange={e => setUnit(e.target.value)}
+                  >
+                    <option>g</option>
+                    <option>kg</option>
+                    <option>mL</option>
+                    <option>liter</option>
+                    <option>meter</option>
+                    <option>item</option>
+                  </Form.Control>
+                </div>
+                <div className="col-sm-6">
+                  <Form.Control type="number" placeholder="Price in Php" />
+                  <Form.Text
+                    value={formPrice}
+                    onChange={e => setFormPrice(e.target.value)}
+                    className="text-muted"
+                  >
+                    Answer price per unit.
+                  </Form.Text>
+                </div>
+              </div>
+            </div>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              placeholder="Place your description here"
+              value={formDescription}
+              onChange={e => setFormDescription(e.target.value)}
+              as="textarea"
+              rows="3"
+            />
+          </Form.Group>
+          <Button
+            onClick={e =>
+              saveSupply(
+                formName,
+                formAddress,
+                formRegion,
+                formAmount,
+                formPrice,
+                formDescription
+              )
+            }
+            variant="primary"
+          >
+            Add Item
+          </Button>
+          <Button type="submit" variant="danger">
+            SUBMIT
+          </Button>
+        </Form>
+
         {supplyList}
         <div className="container">
-            <div className="row">
-                <div className="col-md" style ={{textAlign:"left"}}>
-                    <Button onClick={previous} variant="info">
-                    <svg className="bi bi-arrow-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clipRule="evenodd"/>
-                    <path fillRule="evenodd" d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clipRule="evenodd"/>
-                    </svg>
-                        Previous</Button>
-                </div>
+          <div className="row">
+            <div className="col-md" style={{ textAlign: "left" }}>
+              <Button onClick={previous} variant="info">
+                <svg
+                  className="bi bi-arrow-left"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z"
+                    clipRule="evenodd"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                Previous
+              </Button>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
 
-        {/* <Modal show={show} onHide={handleClose}>
+      {/* <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Submit Form</Modal.Title>
         </Modal.Header>
@@ -202,9 +284,8 @@ const SupplyForm= ({ orgName, firstName,lastName, email,phone,supplies, setSuppl
           </Button>
         </Modal.Footer>
       </Modal> */}
-
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default SupplyForm;
